30ab560f73b440b5d695f45228b7a68c3b522e26
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-29 09:47:15 -0600

    added router

--- lam/routes.js
+++ lam/routes.js
@@ -1,1 +1,22 @@
 var express = require("express");
+var User = require("./models/user");
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.errors = req.flash("error");
+  res.locals.infos = req.flash("infos");
+  next();
+});
+
+router.get("/", function(req, res, next) {
+  User.find()
+    .sort({ createAt: "descending" })
+    .exec(function(err, users) {
+      if (err) { return next(err): }
+      res.render("index", { user: users });
+    });
+});
+
+module.exports = router;
